5074cea2746ac7d71ef61351b51fb80f2f6b8452
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2025-10-07 14:38:00 -0700

    Worked on CheckData.jsx, subject to change

--- src/components/CheckData.jsx
+++ src/components/CheckData.jsx
@@ -1,3 +1,21 @@
 export default function CheckData(props) {
+    let yearInfo = `Active Since: ${props.year}`;
+    let featured = "";
 
+    if (props.year >= 2020) {
+        yearInfo = yearInfo.concat(" (Newer)");
+    } else if (props.year < 2020) {
+        yearInfo = yearInfo.concat(" (Older)");
+    }
+
+    if (props.featured) {
+        featured = "Featured";
+    }
+
+    return (
+        <>
+            <p>{yearInfo}</p>
+            <p>{featured}</p>
+        </>
+    )
 }
