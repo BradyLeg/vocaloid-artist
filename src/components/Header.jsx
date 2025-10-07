6ba1786fea1fcdfaf968542b9830b03748cb2761
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-10-07 13:46:33 -0700

    current

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
     return (
         <header>
-            
+            <h1>Vocaloid Artists</h1>
+        <button onClick={toggleGrid}>
+            {showGrid ? "Hide Album Grid" : "Show Album Grid"}
+        </button>
         </header>
     )
 }
